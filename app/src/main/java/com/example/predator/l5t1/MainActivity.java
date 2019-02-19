f463119af16807b135b0eea999558c44379028a9
Ville <t7kavi01@students.oamk.fi>
2019-02-19 19:34:03 +0200

    Pikku muutos

--- app/src/main/java/com/example/predator/l5t1/MainActivity.java
+++ app/src/main/java/com/example/predator/l5t1/MainActivity.java
@@ -82,8 +82,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if(item.getItemId() == R.id.btnClear)
         {
             clearList();
+            //jooo
         }
-
         return true;
     }
 
